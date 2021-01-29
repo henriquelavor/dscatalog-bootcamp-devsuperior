c0e4c2c55d18b70c360fa234dfc16b50228cba95
henriquelavor <henriquelavor@gmail.com>
2021-01-29 17:00:02 -0400

    Database seeding

--- backend/src/main/java/br/gov/rr/segad/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/gov/rr/segad/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.rr.segad.dscatalog.entities.Category;
 import br.gov.rr.segad.dscatalog.repositories.CategoryRepository;
@@ -23,6 +24,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	// Permite a transação ao banco de dados, e o readOnly evita travar ou locking no banco de dados
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
